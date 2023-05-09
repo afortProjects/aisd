ee05513ef96effead9ec75950403dc11a658df6d
Wojtek <Wojtek@LAPTOP-JT684LJQ>
2023-05-09 18:42:45 +0200

    added lab_3

--- lab_3/main.cpp
+++ lab_3/main.cpp
@@ -2,53 +2,89 @@
 #include <vector>
 #include <string>
 
-int max(int x, int y) {
-   return (x > y) ? x : y;
+using namespace std;
+
+bool dfs(int& i, int parent, vector<bool>& visited, vector<string>& matrix) {
+    visited[i] = true;
+
+    for(int v=0; v<matrix[i].size(); v++) {
+        if(matrix[i][v] == '1') {
+            if(!visited[v]) {
+                if (dfs(v, i, visited, matrix)) 
+                    return true;
+            }
+            else if( v != parent) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
-int knapsack(int capacity, std::vector<int> profits, std::vector<int> weights, int amountOfObjects) {
-   int i, j;
-   int** solutions = new int*[amountOfObjects+1];
-   
-   for(size_t i=0; i<amountOfObjects+1; i++) {
-        solutions[i] = new int[capacity+1];
-   }
+int isCycle(vector<string>& matrix) {
+    size_t n = matrix.size();
+    vector<bool> visited(n, false);
+    for(int i = 0; i<n; i++) {
+        if(!visited[i]) {
+            if(dfs(i, -1, visited, matrix)) 
+                return 1;
+        }
+    }
+    return 0;
+}
 
-   for (i = 0; i <= amountOfObjects; i++) {
-      for (j = 0; j <= capacity; j++) {
-         if (i == 0 || j == 0)
-            solutions[i][j] = 0;
-         else if (weights[i - 1] <= j)
-            solutions[i][j] = max( profits[i-1] + solutions[i - 1][j - weights[i - 1]], solutions[i - 1][j]);
-         else
-            solutions[i][j] = solutions[i - 1][j];
-      }
-   }
-   
-   return solutions[amountOfObjects][capacity];
+bool isConnected(vector<string>& matrix) {
+    size_t n = matrix.size();
+    vector<bool> visited(n, false);
+    int count = 0;
+    for(int i = 0; i<n; i++) {
+        if(!visited[i]) {
+            dfs(i, -1, visited, matrix);
+            count++;
+        }
+    }
+    return count == 1;
+}
+
+bool isSymmetric(vector<string>& matrix) {
+    for(size_t m=0;m<matrix.size(); m++) {
+        for(size_t j=0; j<matrix[m].size(); j++) {
+            if(matrix[m][j] != matrix[j][m])
+                return false;
+        }
+    }
+    return true;
+}
+
+void printMatrix(vector<string>& matrix) {
+    for(size_t i=0;i<matrix.size(); i++) {
+        for(size_t j=0; j<matrix[i].size(); j++) {
+            cout << matrix[i][j];
+        }
+        cout << endl;
+    }
 }
 
 int main() {
-   int amountOfObjects;
-   std::string output = "";
-   while(std::cin>>amountOfObjects) {
-    int capacity;
-    std::vector<int> weights;
-    std::vector<int> profits;
-    for(size_t i = 0;i<amountOfObjects;i++) {
-        int tempProfit;
-        std::cin>>tempProfit;
-        profits.push_back(tempProfit);
-    }
-    for(size_t i = 0;i<amountOfObjects;i++) {
-        int tempWeight;
-        std::cin>>tempWeight;
-        weights.push_back(tempWeight);
-    }
+    int n, length, num;
+    string input, tempString;
+    cin >> n;
 
-    std::cin>>capacity;     
-    output += std::to_string(knapsack(capacity, profits, weights, amountOfObjects)) + '\n';
-   }
-   std::cout<<output;
-   return 0;
+    for(size_t i = 0; i < n ; i++) {
+        vector<string> matrix;
+        cin >> length;
+        cin >> input;
+        for(size_t n = 0; n < input.size(); n++) {
+            if((n+1) % length == 0) {
+                tempString += input[n];
+                matrix.push_back(tempString);
+                tempString = "";
+            } else {
+                tempString += input[n];
+            }
+        }
+    
+        cout << (isCycle(matrix) && isConnected(matrix) && isSymmetric(matrix)) << endl;
+    }
+    return 0;
 }
